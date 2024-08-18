304371b277d306d31fcc5c9057ff05fc35a9f3a1
bhavnamore <164911063+bhavnamore@users.noreply.github.com>
2024-08-19 00:04:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+alert("You can either press keys on keyboard or you can press keys on screen");
+var numofbut=document.querySelectorAll(".drum").length;
+for(var i=0;i<numofbut;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function (){
+        var letter=this.innerHTML;
+        maker(letter);
+        buttonAnimation(letter);
+    })
+}
+
+document.addEventListener("keypress",function(event){
+    maker(event.key);
+    buttonAnimation(event.key);
+})
+
+function buttonAnimation(currentkey){
+    var activekey=document.querySelector("."+ currentkey);
+    activekey.classList.add("pressed");
+    setTimeout(function() {
+        activekey.classList.remove("pressed");
+    },300);
+}
+function maker(key)
+{
+    switch(key)
+        {
+            case "w":
+                var tom1=new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                var tom2=new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "s":
+                var tom3=new Audio("sounds/tom3.mp3");
+                tom3.play();
+                break;
+            case "d":
+                var tom4=new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+            case "j":
+                var crash=new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+            case "k":
+                var kick=new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            case "l":
+                var kick=new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+            default:console.log("letter");  
+        }
+}
+
+
